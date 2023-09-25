0e4e5043b966bcf54d7b17ab0811d466639420c4
skkamalesh <kamalesh0326@gmail.com>
2023-09-26 00:26:49 +0530

    app jsx commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./Components/Sidebar";
 import Dashboard from "./components/Dashboard";
 
 function App() {
